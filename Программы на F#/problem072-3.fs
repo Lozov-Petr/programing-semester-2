b0e527162931697a793ea0a03474932896997041
Лозов Пётр <not_kill@mail.ru>
2013-04-09 21:44:03 +0400

    Задача №72 добавлена.
    Три разных решения.
    Прошу засчитать за три задания, ибо очень много времени и сил на это
    задание потратил, но написал действительно быстрое решение.

---
+++ Программы на F#/problem072-3.fs
@@ -0,0 +1,23 @@
+ (******************************
+           Лозов Пётр
+           Группа 171
+            09.04.13
+           Problem 72 
+      Counting fractions №3
+ Время выполнения - 0.34 секунды
+ *******************************)
+let start = System.DateTime.Now
+let d = 1000000
+let mutable sum = 0L
+let array_fi_number = Array.create (d + 1) 1L
+(* fi - функция Эйлера *)
+for i in 2 .. d do
+    if array_fi_number.[i] = 1L then
+        for j in i .. i .. d do
+            let mutable j' = int64 j
+            while j' % ((int64 i) * (int64 i)) = 0L do
+                array_fi_number.[j] <- array_fi_number.[j] * (int64 i)
+                j' <- j' / (int64 i)
+            array_fi_number.[j] <- array_fi_number.[j] * (int64 (i - 1))
+    sum <- sum + array_fi_number.[i]
+printfn "%A\n%A" sum (System.DateTime.Now - start)  
