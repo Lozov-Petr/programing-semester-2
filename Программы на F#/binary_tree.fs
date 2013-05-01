f1fd93d34715924794c20090b42925a6f0bfffeb
Лозов Пётр <not_kill@mail.ru>
2013-05-02 03:55:12 +0400

    Переделал функцию print_tree.
    И исправил несколько ошибок.

--- Программы на F#/binary_tree.fs
+++ Программы на F#/binary_tree.fs
@@ -1,4 +1,4 @@
-﻿ (******************************
+ (******************************
            Лозов Пётр
            Группа 171
             23.04.13
@@ -36,15 +36,15 @@ let rec delete tree elem =
     (* Если tree = Node(number, Empty, Empty), то заменяем на Leaf number*)
     let node_empty_empty tree =
         match tree with
-        | Node(num, left, right) when left = Empty && right = Empty -> Leaf num
+        | Node(num, Empty, Empty) -> Leaf num
         | _ -> tree
 
     match tree with
     | Leaf num when num = elem -> Empty
     | Node(num, left, right) when elem < num -> node_empty_empty (Node(num, delete left elem, right))
     | Node(num, left, right) when elem > num -> node_empty_empty (Node(num, left, delete right elem))
-    | Node(num, left, right) when left = Empty -> delete right elem
-    | Node(num, left, right) when right = Empty -> left
+    | Node(num, Empty, right) -> delete right elem
+    | Node(num, left, Empty) -> left
     | Node(num, left, right) ->
         
         let rec max_elem_in_tree tree elem =
@@ -57,26 +57,61 @@ let rec delete tree elem =
         delete (Node(max, delete left max, right)) elem 
     | _ -> tree
 
-
 let rec delete_list tree list =
     match list with
     | [] -> tree
     | hd :: tl -> delete_list (delete tree hd) tl
 
 let print_tree tree =
-    let rec print_tree' tree =
-        match tree with
-        | Node(num, left, right) ->
-            printf "%A (" num
-            print_tree' left
-            printf ", " 
-            print_tree' right
-            printf ")" 
-        | Leaf num -> printf "%A" num
-        | Empty -> printf "E"
-    print_tree' tree
-    printfn ""
+    
+    let rec print_tree' tree list acc = 
         
+        let rec print_spaces num =
+            match num with
+            | 1 -> printf " "
+            | _ ->
+                printf " "
+                print_spaces (num - 1)
+
+        let rec print_sticks list =
+            match list with
+            | hd :: [] -> print_spaces hd
+            | hd :: tl ->
+                print_spaces hd
+                printf "|"
+                print_sticks tl
+            | [] -> ()
+
+        match tree with
+        | Empty -> printfn ""
+        | Leaf num -> 
+            if acc > 0 then printfn "\-(%A)" num 
+            else if acc = 0 || acc < 1 then printfn "-(%A)" num
+            else printfn "(%A)" num
+        | Node (num, left, right) ->
+            let acc' = 
+                if acc > 0 then 
+                    printf "\-(%A)*" num
+                    acc + 4 + String.length (string num)
+                else if acc = 0 || acc < -1 then
+                    printf "-(%A)*" num
+                    3 - acc + String.length (string num)
+                else
+                    printf " (%A)*" num
+                    3 + String.length (string num)
+            if left <> Empty then 
+                let list' = list @ [acc']
+                print_tree' right list' 0
+                print_sticks list'
+                printfn "|"
+                print_sticks list'
+                print_tree' left list acc'
+            else
+                 print_tree' right list (-acc' - 1)
+
+    print_tree' tree [] -1 
+    printfn ""
+                                                
 let tree = insert_list (Leaf 0) [-4; 4; -6; -2; 2; 6] 
   
 let test = 
@@ -88,6 +123,15 @@ let test =
     && delete_list tree [0; -4; 4] = Node(-2, Leaf -6, Node(2, Empty, Leaf 6))
 
 
-printfn "test = %A" test
+printfn "test = %A\n" test
 
-print_tree tree
+let list = List.foldBack (fun x acc -> x @ acc) (List.map (fun x -> [x..2 * x..128]) [64; 32; 16; 8; 4; 2; 1]) []
+let tree' = insert_list Empty list
+
+open System
+let rand = Random()
+let tree'' = insert_list Empty [for i in [1..50] -> (rand.Next()) % 100]
+
+print_tree tree''
+
+print_tree tree'
